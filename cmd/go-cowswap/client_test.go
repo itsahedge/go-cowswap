6aca06232f4aaa77e951d39e6987ee38b2961ea4
hedgedon <donpark@pm.me>
2022-11-21 19:47:19 -0500

    Quote req & test

--- cmd/go-cowswap/client_test.go
+++ cmd/go-cowswap/client_test.go
@@ -2,6 +2,8 @@ package go_cowswap
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"github.com/itsahedge/go-cowswap/cmd/go-cowswap/types"
 	"testing"
 )
@@ -21,4 +23,32 @@ func TestNewClient(t *testing.T) {
 		t.Error(err)
 	}
 	t.Logf("status code: %v, response: %v", statusCode, res)
+}
+
+func TestQuote(t *testing.T) {
+	client := NewClient(options)
+	o := &types.QuoteReq{
+		SellToken:           GOERLI_WETH_TOKEN,
+		BuyToken:            GOERLI_COW_TOKEN,
+		Receiver:            "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+		AppData:             "0x0000000000000000000000000000000000000000000000000000000000000000",
+		PartiallyFillable:   false,
+		SellTokenBalance:    "erc20",
+		BuyTokenBalance:     "erc20",
+		PriceQuality:        "fast",
+		SigningScheme:       "eip712",
+		OnchainOrder:        false,
+		Kind:                "sell",
+		SellAmountBeforeFee: "1000000000000000000",
+		From:                "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045",
+	}
+	res, statusCode, err := client.Quote(context.Background(), o)
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := json.MarshalIndent(res, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.Logf("status code: %v\n%v", statusCode, string(r))
 }
---
+++ cmd/go-cowswap/quote.go
@@ -0,0 +1,16 @@
+package go_cowswap
+
+import (
+	"context"
+	"github.com/itsahedge/go-cowswap/cmd/go-cowswap/types"
+)
+
+func (c *Client) Quote(ctx context.Context, o *types.QuoteReq) (*types.QuoteResponse, int, error) {
+	endpoint := "/quote"
+	var dataRes types.QuoteResponse
+	statusCode, err := c.doRequest(ctx, endpoint, "POST", &dataRes, o)
+	if err != nil {
+		return nil, statusCode, err
+	}
+	return &dataRes, statusCode, nil
+}
