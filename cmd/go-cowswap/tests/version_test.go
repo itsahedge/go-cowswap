8d356de27fc39545694f6740c5beaae44dbd5708
itsahedge <donpark@pm.me>
2022-11-25 01:08:41 -0500

    client & version test

---
+++ cmd/go-cowswap/tests/version_test.go
@@ -0,0 +1,17 @@
+package go_cowswap
+
+import (
+	"context"
+	go_cowswap "github.com/itsahedge/go-cowswap/cmd/go-cowswap"
+	"github.com/itsahedge/go-cowswap/cmd/go-cowswap/util"
+	"testing"
+)
+
+func TestClient_GetVersion(t *testing.T) {
+	client, err := go_cowswap.NewClient(util.Options)
+	res, statusCode, err := client.Version(context.Background())
+	if err != nil {
+		t.Fatalf("Version err: %v", err)
+	}
+	t.Logf("status code: %v, response: %v", statusCode, res)
+}
