1d566a8a5ef23162a3849fdd9be7ca492a78cd10
don <donpark@pm.me>
2023-01-05 11:16:35 -0600

    subgraph: custom query handler

---
+++ subgraph/tests/custom_test.go
@@ -0,0 +1,40 @@
+package tests
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/itsahedge/go-cowswap/subgraph"
+	"testing"
+)
+
+func Test_GetCustomQuery(t *testing.T) {
+	gql_client, err := subgraph.NewSubgraphClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+	customQuery := `
+		query users {
+		  users(where: {numberOfTrades_gt: 10, numberOfTrades_lt: 50 }) {
+			id
+			address
+			firstTradeTimestamp
+			isSolver
+			numberOfTrades
+			solvedAmountEth
+			solvedAmountUsd
+			tradedAmountUsd
+			tradedAmountEth
+			ordersPlaced {
+				id
+				isSigned
+			}
+		  }
+		}
+	`
+	resp, err := gql_client.GetCustom(context.Background(), customQuery)
+	if err != nil {
+		t.Fatal(err)
+	}
+	r, _ := json.MarshalIndent(resp, "", "  ")
+	t.Logf("%v", string(r))
+}
