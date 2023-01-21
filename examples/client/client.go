3f814110c30e86d2129452563feaa20008ef1c59
itsahedge <donpark@pm.me>
2023-01-21 12:48:13 -0600

    update: client example

--- examples/client/client.go
+++ examples/client/client.go
@@ -2,26 +2,48 @@ package main
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	cowswap "github.com/itsahedge/go-cowswap"
 	"log"
 )
 
 func main() {
-	// Initialize the go-cowswap client on Goerli with default RPC
+	// Initialize the go-cowswap client requires properly setting the ConfigOpts
+	// Set network: mainnet, goerli, xdai
+	network := "goerli"
+	// Set RPC url
+	rpc := "https://eth-goerli-rpc.gateway.pokt.network"
+	// set host: cowswap api url
+	host := "https://api.cow.fi/goerli/api/v1"
+	// Generate random PrivateKey
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	privateKeyString := hexutil.Encode(privateKeyBytes)[2:]
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	// Set the Config options
 	options := cowswap.ConfigOpts{
-		Network:    "goerli",
-		Host:       cowswap.HostConfig["goerli"],
-		RpcUrl:     cowswap.RpcConfig["goerli"],
-		EthAddress: "YOUR-ETHEREUM-ADDRESS",
-		PrivateKey: "YOUR-PRIVATE-KEY",
+		Network:    network,
+		Host:       host,
+		RpcUrl:     rpc,
+		EthAddress: address,
+		PrivateKey: privateKeyString,
 	}
 	client, err := cowswap.NewClient(options)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
-
 	// Fetch the Chain ID and Block Number from the Client
 	chainId, err := client.EthClient.ChainID(ctx)
 	if err != nil {
